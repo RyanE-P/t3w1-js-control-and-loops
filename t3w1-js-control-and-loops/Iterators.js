0467c7c8d69b07e2d3d7a89a9f017da757ac5126
Ryan_Edwards-Pritchard <ryan@getcape.io>
2024-10-26 21:31:13 +1100

    loops

---
+++ t3w1-js-control-and-loops/Iterators.js
@@ -0,0 +1,53 @@
+const { someMessage, someCoolMessage } = require("./exampleJavaScript");
+
+let fruitChoices = [
+    "organges".at,
+    "bananas",
+    "mangoes"
+
+];
+
+for (let index = 0; index < fruitChoices.length; index++)  {
+    const element = fruitChoices[index];
+    console.log(element);
+    console.log('The fruit of this iteration is ${fruitchoices[index]}');
+}
+// staert at 0, until 0 reaches 4, do a thing and increase 0 up to 1 every time we do that thing
+for (let i = i < 5; i++ ) {
+    console.log('This is the ${i} iteration on the loop');
+    console.log("This is the " + i + " iteration of the loop"); // String concatenation
+
+}
+
+// forEach
+fruitChoices.forEach(element => {
+    console.log(element.toLocaleUpperCase();
+})
+
+//for of
+
+for (const fruit of fruitChoices) {
+    console.log("For of result: " + fruit);
+}
+
+//map -> map returns a new array, and every element has a change applied on it
+
+let fruitChoicesShortened = fruitChoices.map(fruit => {
+    return fruit.substring(0,3);
+})
+
+console.log(fruitChoicesShortened);
+
+//filter -> returns a new array, only with the elements that meet the condition.
+
+let nonCitrusFruits = fruitChoices.filter(fruit => {
+    return fruit != "oranges" && fruit != "lemons"
+})
+
+console.log(nonCitrusFruits);
+if (someMessage === "Bananas") {
+    //                  Bananas        Ba          BA
+    someCoolMessage = someMessage.substring(0, 2).toLocaleUpperCase();
+} else {
+    someCoolMessage = "Default mesage here.";
+}
---
+++ t3w1-js-control-and-loops/exampleJavaScript.js
@@ -0,0 +1,64 @@
+// console.log("Hello!")
+//if and if/else statements
+
+export let someMessage = 10; //"Bananas";
+
+if (someMessage == "Oranges") {
+    //truthy condition
+    console.log("We have oranges");
+}
+
+console.log(someMessage); 
+let someNumber = "10";
+
+if (someNumber ==10) {
+    console.log("We have 10!!")
+}
+
+if (someNumber >= 5 ) {
+    console.log("We have 5 or more!");
+} else {
+    console.log("Less than five");
+}
+
+//ternary operators
+
+export let someCoolMessage = (someMessage == "Bananas") ?
+    ? someMessage.toLocaleUpperCase() 
+    : "Default message here."
+
+console.log(someCoolMessage);
+
+// Switch operator
+
+someMessage = "Orange";
+
+switch (someMessage) {
+    case "orange":
+        console.log("We have oranges");
+        break;
+    case "bananas":
+        console.log("bananas");
+        break;
+    case "mangoes"
+        console.log("mangoes");
+        break;
+    default:
+        console.log("We have another fruit")
+}
+
+//short circuit logic
+
+someMessage = "Mango";
+// evaluate if the first part is true, if it isa, we return the second part 
+// if the first part is false, it returns the first part "", 0, false boolean
+let circuitLogicMessage = someMessage && "We have a message";
+
+console.log(circuitLogicMessage);
+
+// It returns the first block that is true 
+// If both false, it will return the second one
+circuitLogicMessage = "" || 0;
+
+console.log(circuitLogicMessage);
+
